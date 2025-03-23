7b74277d752eeec9bf756f6e4dff594d0602ef90
princejain1198 <princejain1198@gmail.com>
2025-03-23 17:06:35 +0530

    fourth commit

--- src/main/java/net/engineeringdigest/journalApp/utilis/JwtUtil.java
+++ src/main/java/net/engineeringdigest/journalApp/utilis/JwtUtil.java
@@ -52,7 +52,7 @@ public class JwtUtil {
                 .header().empty().add("typ", "JWT")
                 .and()
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60)) // 60 minutes expiration time.
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 60 minutes expiration time.
                 .signWith(getSigningKey())
                 .compact();
     }
