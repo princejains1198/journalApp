d38377e3245605b7a018af33b9db9f4d04a422e3
prince <princejain1198@gmail.com>
2025-04-05 18:32:26 +0530

    first commit of journalApp

--- src/test/java/net/engineeringdigest/journalApp/service/UserDetailsServiceImplTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/UserDetailsServiceImplTests.java
@@ -1,44 +1,44 @@
-package net.engineeringdigest.journalApp.service;
-
-import net.engineeringdigest.journalApp.entity.User;
-import net.engineeringdigest.journalApp.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.ArrayList;
-
-import static org.mockito.Mockito.*;
-
-public class UserDetailsServiceImplTests {
-
-    @InjectMocks
-    private UserDetailsServiceImpl userDetailsService;
-
-    @Mock
-    private UserRepository userRepository;
-
-    @Disabled
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
-
-    @Disabled
-    @Test
-    void loadUserByUserNameTest() {
-        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("ram").password("afaadggas").roles(new ArrayList<>()).build());
-//        when(userRepository.findByUserName("ram")).thenReturn(User.builder().userName("ram").password("afaadggas").build());
-        UserDetails userDetails = userDetailsService.loadUserByUsername("ram");
-        Assertions.assertNotNull(userDetails);
-    }
-}
+//package net.engineeringdigest.journalApp.service;
+//
+//import net.engineeringdigest.journalApp.entity.User;
+//import net.engineeringdigest.journalApp.repository.UserRepository;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.ArgumentMatchers;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.ArrayList;
+//
+//import static org.mockito.Mockito.*;
+//
+//public class UserDetailsServiceImplTests {
+//
+//    @InjectMocks
+//    private UserDetailsServiceImpl userDetailsService;
+//
+//    @Mock
+//    private UserRepository userRepository;
+//
+//    @Disabled
+//    @BeforeEach
+//    void setUp() {
+//        MockitoAnnotations.initMocks(this);
+//    }
+//
+//    @Disabled
+//    @Test
+//    void loadUserByUserNameTest() {
+//        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("ram").password("afaadggas").roles(new ArrayList<>()).build());
+////        when(userRepository.findByUserName("ram")).thenReturn(User.builder().userName("ram").password("afaadggas").build());
+//        UserDetails userDetails = userDetailsService.loadUserByUsername("ram");
+//        Assertions.assertNotNull(userDetails);
+//    }
+//}
