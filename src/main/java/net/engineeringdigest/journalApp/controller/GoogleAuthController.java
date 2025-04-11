ca2879355d70cbe01e2ad4c57831da3eafd26270
prince <princejain1198@gmail.com>
2025-04-12 01:36:44 +0530

    OAuth2 implementation in journalApp

--- src/main/java/net/engineeringdigest/journalApp/controller/GoogleAuthController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/GoogleAuthController.java
@@ -10,8 +10,6 @@ import net.engineeringdigest.journalApp.utilis.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.LinkedMultiValueMap;
